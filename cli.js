ffe550a522ef5039715e23deb4c78db84243fdc7
Antonio Malevski <antonio.malevski@peopleweek.com>
2020-11-19 20:59:42 +0100

    init commit

---
+++ cli.js
@@ -0,0 +1,55 @@
+const fs = require('file-system');
+const {startImport} = require('./import/index.js');
+const {startExport} = require('./export/index.js');
+
+
+const scheduleOptions = {
+    'RUN_EVERY_MINUTE': '* * * * *',
+    'RUN_EVERY_HOUR': '0 * * * *',
+    'RUN_EVERY_TWO_HOURS': '0 */2 * * *',
+    'RUN_EVERY_DAY_AT_MIDNIGHT': '0 0 * * *'
+};
+
+const [, , configFilePath, ...args] = process.argv;
+
+const config = require(configFilePath);
+
+Object.values(config).forEach(feature => {
+
+    if(feature.import){
+        const {scheduleTime, source, destination} = feature.import;
+        const {sftp, variables} = feature;
+
+        let sftpConfig = {
+            ...sftp,
+            privateKey: fs.readFileSync(sftp.privateKeyPath)
+        };
+        delete sftpConfig.privateKeyPath;
+
+        startImport(
+            scheduleOptions.hasOwnProperty(scheduleTime) ? scheduleOptions[scheduleTime] : scheduleTime, 
+            source, 
+            destination,
+            sftpConfig, 
+            variables
+        );
+    }
+    
+    if(feature.export){
+        const {source, destination} = feature.export;
+        const {sftp, variables} = feature;
+
+        let sftpConfig = {
+            ...sftp,
+            privateKey: fs.readFileSync(sftp.privateKeyPath)
+        };
+        delete sftpConfig.privateKeyPath;
+        
+        startExport(
+            source, 
+            destination,
+            sftpConfig,
+            variables
+        );
+    }
+})
---
+++ import/index.js
@@ -0,0 +1,98 @@
+var cron = require('node-cron');
+const fs = require('file-system');
+const imported_files = require('./imported_files.json');
+
+let Client = require('ssh2-sftp-client');
+
+const replaceVariablesInPath = (path, variables) => {
+  let replacedPath = path;
+  Object.keys(variables).forEach((key) => {
+      replacedPath = path.split(key).join(variables[key]);
+  })
+  return replacedPath;
+}
+
+const addFileToImportedFiles = (file, destination, status) => {
+    let importedFilesUpdated = {...imported_files};
+    importedFilesUpdated[destination + '/' + file.name] = {
+        successfully: status
+    };
+
+    fs.writeFile('./import/imported_files.json', 
+        JSON.stringify(importedFilesUpdated), 
+        (err) => {
+          if (err) return console.log(err);
+    });
+}
+
+const downloadFiles = (files, source, destination, sftpConfig) => {
+    files.forEach(file => {
+          let sftp = new Client();
+
+          sftp.connect(sftpConfig).then(() => {
+            return sftp.exists(source + "/" + file.name);
+          })
+          .then((exists) => {
+              if(exists){
+                  if(!fs.existsSync(destination)){
+                      fs.mkdirSync(destination, { recursive: true });
+                  }
+                  return sftp.fastGet(
+                      source + '/' + file.name,
+                      destination + '/' + file.name
+                    );
+              } else {
+                  return sftp.end();
+              }
+          })
+          .then((res) => {
+              if(res.includes('successfully')) {
+                addFileToImportedFiles(file, destination, true);
+              }
+              else {
+                addFileToImportedFiles(file, destination, false);
+              }
+              return sftp.end();
+          })
+          .catch(err => {
+              addFileToImportedFiles(file, destination, false);
+              console.error(err.message);
+          });
+    });
+}
+
+
+exports.startImport = (scheduleTime, source, destination, sftpConfig, variables) => {
+    const destinationPath = replaceVariablesInPath(destination, variables);
+    cron.schedule(scheduleTime, () => {
+        console.log('try');
+
+        let sftp = new Client();
+        sftp.connect(sftpConfig).then(() => {
+          return sftp.exists(source);
+        })
+        .then((exists) => {
+            if(exists){
+                return sftp.list(source);
+            } else {
+                return sftp.end();
+            }
+        })
+        .then((files) => {
+            if(!!files && !!files.length) {
+                let filesToDownload = files.filter(file => {
+                    const key = destinationPath + '/' + file.name;
+                    return !imported_files.hasOwnProperty(key) 
+                          || !imported_files[key].successfully;
+                })
+                if(!!filesToDownload && !!filesToDownload.length){
+                    downloadFiles(filesToDownload, source, destinationPath, sftpConfig);
+                }
+            }
+            return sftp.end();
+        })
+        .catch(err => {
+            console.error(err.message);
+        });
+    });
+}
